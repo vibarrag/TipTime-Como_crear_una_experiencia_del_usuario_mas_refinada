916506920d90aa7fe6dc40cd1e62bb9d5e280afb
Victor <vibarrag@unirioja.es>
2022-03-03 11:53:22 +0100

    Ejercicio "Cómo crear una experiencia del usuario más refinada" TERMINADO

--- app/src/main/java/com/example/tiptime2/MainActivity.kt
+++ app/src/main/java/com/example/tiptime2/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.tiptime2
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tiptime2.R
 import com.example.tiptime2.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -13,10 +16,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inflate the layout XML file and return a binding object instance
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Set the content view of the Activity to be the root view of the layout
         setContentView(binding.root)
 
+        // Setup a click listener on the calculate button to calculate the tip
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        // Set up a key listener on the EditText field to listen for "enter" button presses
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
     }
 
     private fun calculateTip() {
@@ -41,4 +56,17 @@ class MainActivity : AppCompatActivity() {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+
 }
